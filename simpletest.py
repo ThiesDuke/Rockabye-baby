2451c9c3f67317ab8ee177e9e3cdae618cf1294a
thies <thies@192.168.1.100>
2017-02-27 07:53:48 +0100

    axis definition

--- simpletest.py
+++ simpletest.py
@@ -4,9 +4,19 @@
 # License: Public Domain
 from __future__ import division
 import time
-
-# Import the PCA9685 module.
+from adxl345 import ADXL345
+from _ast import Or
+adxl345 = ADXL345()
 import Adafruit_PCA9685
+
+def check_position():
+    axes = adxl345.getAxes(True)
+    #print "ADXL345 on address 0x%x:" % (adxl345.address)
+    print "   x = %.3fG" % ( axes['x'] )
+    print "   y = %.3fG" % ( axes['y'] )
+    print "   z = %.3fG" % ( axes['z'] )
+    position = [axes['x'],axes['y'],axes['z']]
+    return position
 
 
 # Uncomment to enable debug output.
@@ -20,8 +30,9 @@ pwm = Adafruit_PCA9685.PCA9685()
 #pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
 
 # Configure min and max servo pulse lengths
-servo_min = 150  # Min pulse length out of 4096
+servo = 100  # Min pulse length out of 4096
 servo_max = 600  # Max pulse length out of 4096
+old_servo = 100
 
 # Helper function to make setting a servo pulse width simpler.
 def set_servo_pulse(channel, pulse):
@@ -40,7 +51,16 @@ pwm.set_pwm_freq(60)
 print('Moving servo on channel 0, press Ctrl-C to quit...')
 while True:
     # Move servo on channel O between extremes.
-    pwm.set_pwm(0, 0, servo_min)
-    time.sleep(1)
-    pwm.set_pwm(0, 0, servo_max)
-    time.sleep(1)
+    servo = check_position()
+    servo = int(abs(servo[0])*400)
+    print(servo)
+    #print("############################")
+    #print("                            ")
+    if old_servo+50 < servo or old_servo-50 > servo :
+        #pwm.set_pwm(0, 0, servo)
+        print(old_servo)
+        #print("Set servo")
+        old_servo = servo
+    time.sleep(0.3)
+    #pwm.set_pwm(0, 0, servo_max)
+    #time.sleep(0.5)
